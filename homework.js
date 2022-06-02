e7536543147d9d24da5737619c0f65ca93ae8c0f
GaleArt <GaleguzovAA@gmail.com>
2022-06-02 23:52:28 +0300

    Тестирую js к меню навигации

--- homework.js
+++ homework.js
@@ -5,3 +5,11 @@ function showMenu() {
 function closeMenu() {
     document.getElementById('menu').style.display = "none";
 }
+
+/* Тест выделения в меню */
+
+$(document).ready(function(){
+    $('#gmenu > ul > li >a').each(function () {
+        if($(this).attr('href') == location.href) $(this).addClass('Selected');
+    });
+});
