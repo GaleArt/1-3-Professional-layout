5b01a8c60f7deededc882426c0bc680438049194
GaleArt <GaleguzovAA@gmail.com>
2022-06-03 22:40:27 +0300

    Добавил анимацию banner + запилил общую анимацию для кнопок

--- homework.js
+++ homework.js
@@ -5,11 +5,3 @@ function showMenu() {
 function closeMenu() {
     document.getElementById('menu').style.display = "none";
 }
-
-/* Тест выделения в меню */
-
-$(document).ready(function(){
-    $('#gmenu > ul > li >a').each(function () {
-        if($(this).attr('href') == location.href) $(this).addClass('Selected');
-    });
-});
